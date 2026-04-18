8ef46c04a05174a818e876d931f7a19f0476786d
amazinmays1 <amazinmays@gmail.com>
2026-04-18 15:50:37 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HeyfroseWebsite from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HeyfroseWebsite />
+  </React.StrictMode>
+);
